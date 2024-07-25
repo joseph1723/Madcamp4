0e24ce809a199cd0045d4d7ba442e36c09890c83
mummy-alive <mummyee@ewhain.net>
2024-07-25 17:39:50 +0900

    add background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.glide)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
